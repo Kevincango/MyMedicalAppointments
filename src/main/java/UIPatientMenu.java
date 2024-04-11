cf5812f26606db1996b618a6dcb21c4bbd6c38cf
Kevin Cantu Gomez <61637162+Kevincango@users.noreply.github.com>
2024-04-11 23:44:50 +0000

    showBookAppointments logic and arrayList iterations

--- src/main/java/UIPatientMenu.java
+++ src/main/java/UIPatientMenu.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.ArrayList;
 
 public class UIPatientMenu {
 
@@ -33,6 +36,24 @@ public class UIPatientMenu {
     do{
       System.out.println(":: Book an appointment ::");
       System.out.println("1. Select a date");
+      //dates list
+      Map<Integer, Map<Integer, Doctor>> doctors = new TreeMap<Integer, Map<Integer, Doctor>>();
+      int k = 0;
+      for(int i = 0; i < UIDoctorMenu.doctorsAvailableAppointments.size() ; i++){
+        ArrayList<Doctor.AvailableAppointment> availableAppointments = UIDoctorMenu.doctorsAvailableAppointments.get(i).getAvailableAppointments();
+        Map<Integer, Doctor> doctorAppointments = new TreeMap<Integer, Doctor>();
+
+        for(int j = 0; j < availableAppointments.size(); j++){
+          k++;
+          System.out.println(k + " " +  availableAppointments.get(j).getDate());
+        
+          doctorAppointments.put(Integer.valueOf(j), UIDoctorMenu.doctorsAvailableAppointments.get(i));
+
+          doctors.put(Integer.valueOf(k), doctorAppointments);
+        }
+      }
+      Scanner sc = new Scanner(System.in);
+      int responseDateSelected = Integer.valueOf(sc.nextLine());
     }while(response != 0);
   }
 }
